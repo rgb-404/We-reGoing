56950a424625e39701e15b9bbfc977a7543b9d50
raghav <[bhupatiraju.raghav@gmail.com]>
2025-04-05 16:04:52 +0100

    added base 6 egg times + starting time functionality, need to continue

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// the egg times
+let halfboilTime = 450
+let softboilTime = 450
+let medboilTime = 450
+let hardboilTime = 450
+let steamTime = 450
+let poachTime = 450
+
+
+// misc variables
+let timer;
+let secsRemaining;
+let timerOn = false;
+
+
+
+// redirects the current window to the chosen egg timer
 function EggPage(eggType) {
     switch(eggType) {
         case 'landing':
@@ -22,4 +39,56 @@ function EggPage(eggType) {
             window.location.href = 'poached.html';
             break;
     }
+}
+
+function startTimer() {
+    switch(eggType) {
+        case 'half':
+            secsRemaining = halfboilTime
+            break;
+        case 'soft':
+            secsRemaining = softboilTime
+            break;
+        case 'med':
+            secsRemaining = medboilTime
+            break;
+        case 'hard':
+            secsRemaining = hardboilTime
+            break;
+        case 'steam':
+            secsRemaining = steamTime
+            break;
+        case 'poach':
+            secsRemaining = poachTime
+            break;
+        default:
+            secsRemaining = 0
+            break;
+    }
+
+    
+    
+    if (isTimerRunning) return;
+
+
+
+    timer = setInterval(function() {
+        
+    }
+
+
+    ) 
+}
+
+
+
+
+
+
+function pauseTimer() {
+    
+}
+
+function snoozeTimer() {
+    
 }
