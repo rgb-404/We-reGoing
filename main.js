bd33d33a435b84d0685c1de09994ce4510d46a43
raghav <[bhupatiraju.raghav@gmail.com]>
2025-09-05 23:14:08 +0800

    uploading all

---
+++ main.js
@@ -0,0 +1,32 @@
+function EggPage(eggType) {
+    switch(eggType) {
+        case 'landing':
+            window.location.href = 'landing.html';
+            break;
+        case 'half':
+            window.location.href = 'halfboiled.html';
+            break;
+        case 'soft':
+            window.location.href = 'softboiled.html';
+            break;
+        case 'hard':
+            window.location.href = 'hardboiled.html';
+            break;
+        case 'fried':
+            window.location.href = 'fried.html';
+            break;
+        case 'scrambled':
+            window.location.href = 'scrambled.html';
+            break;
+        case 'poached':
+            window.location.href = 'poached.html';
+            break;
+    }
+}
+
+
+function backPage(backFrom) {
+    if (backFrom == 'landing') {
+        window.location.href = 'index.html';
+    } else window.location.href = 'landing.html';
+}
